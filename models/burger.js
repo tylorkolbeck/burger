3e90e968660384e1a632c4f25c4e65667c10860c
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-16 18:09:34 -0700

    add model

---
+++ models/burger.js
@@ -0,0 +1,34 @@
+const orm = require("../config/orm")
+
+const burger = {
+  selectAll: (cb) => {
+    orm.selectAll("burgers", (result) => {
+      cb(result)
+    })
+  },
+  insertOne: (values, cb) => {
+    orm.insertOne("burgers", values, (result) => {
+      cb(result)
+    })
+  },
+
+  updateOne: (columnName, value, id, cb) => {
+    orm.updateOne("burgers", columnName, value, id, (results) => {
+      cb(results)
+    })
+  },
+}
+
+// burger.updateOne("burger_name", "New Burger Name1", 7, (result) => {
+//   console.log(result)
+// })
+
+// burger.insertOne({ burger_name: "Yum Yum Burger", devoured: true }, (result) =>
+//   console.log(result)
+// )
+
+// burger.selectAll((results) => {
+//   console.log(results)
+// })
+
+module.exports = burger
