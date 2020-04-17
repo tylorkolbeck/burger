d1dc333fac327c85fa824e47df75ac50a5192370
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-17 09:27:19 -0700

    remove comments

--- models/burger.js
+++ models/burger.js
@@ -13,22 +13,10 @@ const burger = {
   },
 
   updateOne: (value, id, cb) => {
-    orm.updateOne("burgers", "devoured", value, id, (results) => {
+    orm.updateOne("burgers", { devoured: value }, id, (results) => {
       cb(results)
     })
   },
 }
-
-// burger.updateOne("burger_name", "New Burger Name1", 7, (result) => {
-//   console.log(result)
-// })
-
-// burger.insertOne({ burger_name: "Yum Yum Burger", devoured: true }, (result) =>
-//   console.log(result)
-// )
-
-// burger.selectAll((results) => {
-//   console.log(results)
-// })
 
 module.exports = burger
