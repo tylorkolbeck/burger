0249b48635035eacc26fa3a8c995515c4bf97626
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-16 17:59:08 -0700

    add schema and seed

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+  id int
+  AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR
+  (255) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY
+  (id)
+); 
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ("Burger Name 1", false),
+  ("Burger Name 2", false),
+  ("Burger Name 3", true),
+  ("Burger Name 4", true); 
