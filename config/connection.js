84f855262a8cd2026b06ba761a570a701c7239f6
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-17 09:37:52 -0700

    update connection to handle JAWS DB connection on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 var mysql = require("mysql")
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "password",
-  database: "burgers_db",
-})
+let connection
 
-connection.connect(function (err) {
-  console.log(err || "Connected To Database.")
-})
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "password",
+    database: "burgers_db",
+  })
+}
+
+connection.connect()
 
 module.exports = connection
