50bb7fc9c0ae42e0ed0d47b0336dd7b22047f6ff
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-16 17:58:45 -0700

    add mysql connection and export

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql")
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: "quotes_db",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "password",
+  database: "burgers_db",
 })
 
 connection.connect(function (err) {
